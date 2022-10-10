a7597b84fe658e59c40c52a1b39d9c371d290c25
Brunno Morokuma <BrunnoMorokuma1234@Gmail.com>
2022-10-10 16:53:46 -0300

    my first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { AddNote } from "./components/AddNote";
+import { Home } from "./pages/Home";
+
+function App() {
+
+  return (
+    <div>
+      <AddNote/>
+    </div>
+  );
+}
+
+export default App;
